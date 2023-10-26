4da626b5a28764bbb07b0d761a4e6b36e946ee07
YassineAssim23 <assimy@sheridancollege.ca>
2023-10-26 01:45:26 -0400

    STYLE
    dashboard and recentlyplayed styling

--- src/spotify_utils.py
+++ src/spotify_utils.py
@@ -3,6 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 # Spotify API credentials
 load_dotenv()
@@ -11,52 +12,60 @@ CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 REDIRECT_URI = "http://127.0.0.1:5000/redirectPage"
 
-def create_spotify_client(token_info):
-    return spotipy.Spotify(auth=token_info['access_token'])
 
+def create_spotify_client(token_info):
+    return spotipy.Spotify(auth=token_info["access_token"])
 
 
 def get_user_top_tracks_medium_term(token_info, limit=50):
     return get_user_top_tracks(token_info, limit=limit, time_range="medium_term")
 
+
 def get_user_top_tracks_short_term(token_info, limit=50):
     return get_user_top_tracks(token_info, limit=limit, time_range="short_term")
 
+
 def get_user_top_tracks_long_term(token_info, limit=50):
     return get_user_top_tracks(token_info, limit=limit, time_range="long_term")
+
 
 def get_user_top_tracks(token_info, limit=50, time_range="medium_term"):
     sp = create_spotify_client(token_info)
     try:
         top_tracks = sp.current_user_top_tracks(limit=limit, time_range=time_range)
         tracks_info = []
-        for track in top_tracks['items']:
-            track_name = track['name']
-            artist_name = track['artists'][0]['name']
-            album_name = track['album']['name']
-            album_link = track['album']['external_urls']['spotify']
-            track_link = track['external_urls']['spotify']
-            image_url = track['album']['images'][0]['url'] if track['album']['images'] else None
-            tracks_info.append({
-                'album_name': album_name,
-                'album_link': album_link,
-                'track_name': track_name,
-                'artist_name': artist_name,
-                'track_link': track_link,
-                'image_url': image_url
-            })
+        for track in top_tracks["items"]:
+            track_name = track["name"]
+            artist_name = track["artists"][0]["name"]
+            album_name = track["album"]["name"]
+            album_link = track["album"]["external_urls"]["spotify"]
+            track_link = track["external_urls"]["spotify"]
+            image_url = (
+                track["album"]["images"][0]["url"] if track["album"]["images"] else None
+            )
+            tracks_info.append(
+                {
+                    "album_name": album_name,
+                    "album_link": album_link,
+                    "track_name": track_name,
+                    "artist_name": artist_name,
+                    "track_link": track_link,
+                    "image_url": image_url,
+                }
+            )
         return tracks_info
     except Exception as e:
         print(f"Error getting top tracks: {e}")
         return None
 
 
-
 def get_user_top_artists_medium_term(token_info, limit=50):
     return get_user_top_artists(token_info, limit=limit, time_range="medium_term")
 
+
 def get_user_top_artists_short_term(token_info, limit=50):
     return get_user_top_artists(token_info, limit=limit, time_range="short_term")
+
 
 def get_user_top_artists_long_term(token_info, limit=50):
     return get_user_top_artists(token_info, limit=limit, time_range="long_term")
@@ -64,20 +73,26 @@ def get_user_top_artists_long_term(token_info, limit=50):
 
 def get_user_top_artists(token_info, limit=50):
     # Get the selected time range from the URL parameters
-    selected_time_range = request.args.get('time_range', default='medium_term', type=str)
+    selected_time_range = request.args.get(
+        "time_range", default="medium_term", type=str
+    )
     sp = create_spotify_client(token_info)
     try:
-        top_artists = sp.current_user_top_artists(limit=limit, time_range=selected_time_range)
+        top_artists = sp.current_user_top_artists(
+            limit=limit, time_range=selected_time_range
+        )
         artists_info = []
-        for artist in top_artists['items']:
-            artist_name = artist['name']
-            artist_link = artist['external_urls']['spotify']
-            image_url = artist['images'][0]['url'] if artist['images'] else None
-            artists_info.append({
-                'artist_name': artist_name,
-                'artist_link': artist_link,
-                'image_url': image_url
-            })
+        for artist in top_artists["items"]:
+            artist_name = artist["name"]
+            artist_link = artist["external_urls"]["spotify"]
+            image_url = artist["images"][0]["url"] if artist["images"] else None
+            artists_info.append(
+                {
+                    "artist_name": artist_name,
+                    "artist_link": artist_link,
+                    "image_url": image_url,
+                }
+            )
         return artists_info
     except Exception as e:
         print(f"Error getting top tracks: {e}")
@@ -92,43 +107,59 @@ def get_user_info(token_info):
     except Exception as e:
         print(f"Error getting user information: {e}")
         return None
-    
+
 
 def get_user_devices(token_info):
     sp = create_spotify_client(token_info)
     try:
         devices = sp.devices()
-        return devices.get('devices', [])
+        return devices.get("devices", [])
     except Exception as e:
         print(f"Error getting user devices: {e}")
         return None
 
+
 def get_recently_played(token_info):
     sp = create_spotify_client(token_info)
     try:
-        recently_played = sp.current_user_recently_played(limit=50, after=None, before=None)
-        
-        if 'items' in recently_played:
+        recently_played = sp.current_user_recently_played(
+            limit=50, after=None, before=None
+        )
+
+        if "items" in recently_played:
             tracks_info = []
-            for track in recently_played['items']:
-                track_name = track['track']['name']
-                artist_name = track['track']['artists'][0]['name']
-                album_name = track['track']['album']['name']
-                played_at = track['played_at']
-                track_link = track['track']['external_urls']['spotify']
-                image_url = track['track']['album']['images'][0]['url'] if track['track']['album']['images'] else None
-                tracks_info.append({
-                    'name': track_name,
-                    'artist': artist_name,
-                    'album': album_name,
-                    'played_at': played_at,
-                    'track_link': track_link,
-                    'image_url': image_url
-                })
+            for track in recently_played["items"]:
+                track_name = track["track"]["name"]
+                artist_name = track["track"]["artists"][0]["name"]
+                album_name = track["track"]["album"]["name"]
+
+                # Convert the timestamp to the desired format (M:D:Y) (HH:MM:SS)
+                played_at = datetime.strptime(
+                    track["played_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                ).strftime("%m/%d/%Y %H:%M:%S")
+
+                track_link = track["track"]["external_urls"]["spotify"]
+                image_url = (
+                    track["track"]["album"]["images"][0]["url"]
+                    if track["track"]["album"]["images"]
+                    else None
+                )
+
+                tracks_info.append(
+                    {
+                        "name": track_name,
+                        "artist": artist_name,
+                        "album": album_name,
+                        "played_at": played_at,
+                        "track_link": track_link,
+                        "image_url": image_url,
+                    }
+                )
             return tracks_info
         else:
             print("Unexpected response format from Spotify API:", recently_played)
             return None
+
     except Exception as e:
         print(f"Error getting recently played tracks: {e}")
         return None
@@ -140,21 +171,14 @@ def get_user_playlists(token_info, limit=50, offset=0):
     try:
         playlists = sp.current_user_playlists(limit=limit, offset=offset)
         playlists_info = []
-        for playlist in playlists['items']:
-            playlist_name = playlist['name']
-            playlist_link = playlist['external_urls']['spotify']
-            image_url = playlist['images'][0]['url'] if playlist['images'] else None
-            playlists_info.append({
-                'name': playlist_name,
-                'link': playlist_link,
-                'image_url': image_url
-            })
+        for playlist in playlists["items"]:
+            playlist_name = playlist["name"]
+            playlist_link = playlist["external_urls"]["spotify"]
+            image_url = playlist["images"][0]["url"] if playlist["images"] else None
+            playlists_info.append(
+                {"name": playlist_name, "link": playlist_link, "image_url": image_url}
+            )
         return playlists_info
     except Exception as e:
         print(f"Error getting user playlists: {e}")
         return None
-
-
-
-
-
