4c8cff1ef548a175c10bf12c0c11e213a203aa7d
YassineAssim23 <assimy@sheridancollege.ca>
2023-10-26 04:49:07 -0400

    Comments

--- src/app.py
+++ src/app.py
@@ -7,51 +7,59 @@ import os
 
 load_dotenv()
 
+# Load client ID and client secret from environment variables
 CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-# Define TOKEN_INFO constant
+# Define TOKEN_INFO constant for session storage
 TOKEN_INFO = 'token_info'
 
+# Function to create Spotify OAuth object
 def create_spotify_oauth():
     return SpotifyOAuth(
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
         redirect_uri="http://127.0.0.1:5000/redirectPage", 
-        scope="user-top-read user-library-read user-read-recently-played user-read-playback-state"
+        scope="user-top-read user-library-read user-read-recently-played user-read-playback-state user-read-private user-read-email user-follow-read"
     )
 
 app = Flask(__name__)
 
-
+# Static files route for custom fonts
 @app.route('/fonts/<path:filename>')
 def custom_static(filename):
     root_dir = os.path.dirname(os.getcwd())  # Get the parent directory of your Flask app
     return send_from_directory(os.path.join(root_dir, 'src', 'templates', 'fonts'), filename)
 
-    
+# Disable caching for all responses
 @app.after_request
 def add_header(response):
     response.cache_control.no_store = True
     return response
 
+# Set Flask secret key for session management
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
+# Homepage route
 @app.route('/')
 def homepage():
     return render_template('index.html', title='Welcome to MelodyCapsule!')
 
+# Dashboard route for user information and devices
 @app.route('/dashboard')
 def dashboard():
     user_token = get_token()
+    
     if user_token:
         # Get user information and devices separately
         user_info = spotify_utils.get_user_info(user_token)
+        followers_count = spotify_utils.get_user_followers(user_token)
+        following_count = spotify_utils.get_user_following(user_token)
         devices = spotify_utils.get_user_devices(user_token)
 
         if user_info is not None and devices is not None:
             # If both user information and devices are retrieved successfully, pass them to the template
-            return render_template('dashboard.html', title='Welcome to MelodyCapsule!', user_info=user_info, devices=devices)
+            return render_template('dashboard.html', title='Welcome to MelodyCapsule!', user_info=user_info, followers_count=followers_count, following_count=following_count, devices=devices)
         else:
             # Handle the case when either user information or devices retrieval fails
             error_message = "Error retrieving user information or devices. Please try again later."
@@ -59,21 +67,15 @@ def dashboard():
     else:
         # Handle the case when the user is not authenticated
         return render_template('error.html', error_message="User not authenticated. Please login first.")
-
-
-
+        
+# Login route, redirects to Spotify authorization page
 @app.route('/login')
 def login():
-    sp_oauth = SpotifyOAuth(
-        client_id = CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri="http://127.0.0.1:5000/redirectPage",
-        scope="user-top-read user-library-read user-read-recently-played user-read-playback-state"
-    )
+    sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
-
+# Logout route, clears session data
 @app.route('/logout')
 def logout():
     # Clear the session data (token information)
@@ -81,29 +83,21 @@ def logout():
     # Redirect the user to the homepage after logout
     return redirect(url_for('homepage'))
 
-
-
+# Redirect route after Spotify authorization
 @app.route('/redirectPage')
 def redirectPage():
     code = request.args.get('code')
-    sp_oauth = SpotifyOAuth(
-        client_id = CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri="http://127.0.0.1:5000/redirectPage",
-        scope="user-top-read user-library-read user-read-recently-played user-read-playback-state"
-    )
+    sp_oauth = create_spotify_oauth()
     token_info = sp_oauth.get_access_token(code)
     session[TOKEN_INFO] = token_info
     return redirect(url_for("dashboard"))
 
-
+# Function to retrieve user token from session
 def get_token():
     token_info = session.get(TOKEN_INFO, None)
     return token_info
 
-# Assuming you have a 'dashboard' route defined like this
-from flask import request
-
+# Route to display user's top tracks
 @app.route('/toptracks')
 def toptracks():
     user_token = get_token()
@@ -125,8 +119,7 @@ def toptracks():
         error_message = "User not authenticated. Please login first."
         return render_template('error.html', error_message=error_message)
 
-    
-# Assuming you have a 'dashboard' route defined like this
+# Route to display user's top artists
 @app.route('/topartists')
 def topartists():
     user_token = get_token()
@@ -142,8 +135,7 @@ def topartists():
         error_message = "User not authenticated. Please login first."
         return render_template('error.html', error_message=error_message)
 
-
-# Assuming you have a 'dashboard' route defined like this
+# Route to display user's recently played tracks
 @app.route('/recentlyplayed')
 def recentlyplayed():
     user_token = get_token()
@@ -158,7 +150,7 @@ def recentlyplayed():
         error_message = "User not authenticated. Please login first."
         return render_template('error.html', error_message=error_message)
 
-
+# Route to display user's playlists
 @app.route('/playlist')
 def playlists():
     user_token = get_token()
@@ -172,6 +164,3 @@ def playlists():
     else:
         error_message = "User not authenticated. Please login first."
         return render_template('error.html', error_message=error_message)
-
-
-
--- src/spotify_utils.py
+++ src/spotify_utils.py
@@ -5,36 +5,30 @@ from dotenv import load_dotenv
 import os
 from datetime import datetime
 
-# Spotify API credentials
+# Load Spotify API credentials from environment variables
 load_dotenv()
 
+# Get Spotify API credentials from environment variables
 CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 REDIRECT_URI = "http://127.0.0.1:5000/redirectPage"
 
 
+# Function to create a Spotify client using token information
 def create_spotify_client(token_info):
     return spotipy.Spotify(auth=token_info["access_token"])
 
 
-def get_user_top_tracks_medium_term(token_info, limit=50):
-    return get_user_top_tracks(token_info, limit=limit, time_range="medium_term")
-
-
-def get_user_top_tracks_short_term(token_info, limit=50):
-    return get_user_top_tracks(token_info, limit=limit, time_range="short_term")
-
-
-def get_user_top_tracks_long_term(token_info, limit=50):
-    return get_user_top_tracks(token_info, limit=limit, time_range="long_term")
-
-
+# Function to get the user's top tracks for a specific time range
 def get_user_top_tracks(token_info, limit=50, time_range="medium_term"):
+    # Create a Spotify client using token information
     sp = create_spotify_client(token_info)
     try:
+        # Get the user's top tracks based on the specified time range
         top_tracks = sp.current_user_top_tracks(limit=limit, time_range=time_range)
         tracks_info = []
         for track in top_tracks["items"]:
+            # Extract track information
             track_name = track["name"]
             artist_name = track["artists"][0]["name"]
             album_name = track["album"]["name"]
@@ -43,6 +37,7 @@ def get_user_top_tracks(token_info, limit=50, time_range="medium_term"):
             image_url = (
                 track["album"]["images"][0]["url"] if track["album"]["images"] else None
             )
+            # Add track information to the list
             tracks_info.append(
                 {
                     "album_name": album_name,
@@ -53,39 +48,33 @@ def get_user_top_tracks(token_info, limit=50, time_range="medium_term"):
                     "image_url": image_url,
                 }
             )
+        # Return the list of top tracks
         return tracks_info
     except Exception as e:
         print(f"Error getting top tracks: {e}")
         return None
 
 
-def get_user_top_artists_medium_term(token_info, limit=50):
-    return get_user_top_artists(token_info, limit=limit, time_range="medium_term")
-
-
-def get_user_top_artists_short_term(token_info, limit=50):
-    return get_user_top_artists(token_info, limit=limit, time_range="short_term")
-
-
-def get_user_top_artists_long_term(token_info, limit=50):
-    return get_user_top_artists(token_info, limit=limit, time_range="long_term")
-
-
+# Function to get the user's top artists for a specific time range
 def get_user_top_artists(token_info, limit=50):
     # Get the selected time range from the URL parameters
     selected_time_range = request.args.get(
         "time_range", default="medium_term", type=str
     )
+    # Create a Spotify client using token information
     sp = create_spotify_client(token_info)
     try:
+        # Get the user's top artists based on the specified time range
         top_artists = sp.current_user_top_artists(
             limit=limit, time_range=selected_time_range
         )
         artists_info = []
         for artist in top_artists["items"]:
+            # Extract artist information
             artist_name = artist["name"]
             artist_link = artist["external_urls"]["spotify"]
             image_url = artist["images"][0]["url"] if artist["images"] else None
+            # Add artist information to the list
             artists_info.append(
                 {
                     "artist_name": artist_name,
@@ -93,25 +82,33 @@ def get_user_top_artists(token_info, limit=50):
                     "image_url": image_url,
                 }
             )
+        # Return the list of top artists
         return artists_info
     except Exception as e:
-        print(f"Error getting top tracks: {e}")
+        print(f"Error getting top artists: {e}")
         return None
 
 
+# Function to get the user's information
 def get_user_info(token_info):
+    # Create a Spotify client using token information
     sp = create_spotify_client(token_info)
     try:
+        # Get the user's information
         user_info = sp.current_user()
+        # Return the user's information
         return user_info
     except Exception as e:
         print(f"Error getting user information: {e}")
         return None
 
 
+# Function to get the user's available devices
 def get_user_devices(token_info):
+    # Create a Spotify client using token information
     sp = create_spotify_client(token_info)
     try:
+        # Get the user's available devices
         devices = sp.devices()
         return devices.get("devices", [])
     except Exception as e:
@@ -119,9 +116,12 @@ def get_user_devices(token_info):
         return None
 
 
+# Function to get the user's recently played tracks
 def get_recently_played(token_info):
+    # Create a Spotify client using token information
     sp = create_spotify_client(token_info)
     try:
+        # Get the user's recently played tracks
         recently_played = sp.current_user_recently_played(
             limit=50, after=None, before=None
         )
@@ -129,22 +129,21 @@ def get_recently_played(token_info):
         if "items" in recently_played:
             tracks_info = []
             for track in recently_played["items"]:
+                # Extract recently played track information
                 track_name = track["track"]["name"]
                 artist_name = track["track"]["artists"][0]["name"]
                 album_name = track["track"]["album"]["name"]
-
                 # Convert the timestamp to the desired format (M:D:Y) (HH:MM:SS)
                 played_at = datetime.strptime(
                     track["played_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
                 ).strftime("%m/%d/%Y %H:%M:%S")
-
                 track_link = track["track"]["external_urls"]["spotify"]
                 image_url = (
                     track["track"]["album"]["images"][0]["url"]
                     if track["track"]["album"]["images"]
                     else None
                 )
-
+                # Add recently played track information to the list
                 tracks_info.append(
                     {
                         "name": track_name,
@@ -155,6 +154,7 @@ def get_recently_played(token_info):
                         "image_url": image_url,
                     }
                 )
+            # Return the list of recently played tracks
             return tracks_info
         else:
             print("Unexpected response format from Spotify API:", recently_played)
@@ -165,20 +165,53 @@ def get_recently_played(token_info):
         return None
 
 
-# Update get_user_playlists function in spotify_utils.py
+# Function to get the user's playlists
 def get_user_playlists(token_info, limit=50, offset=0):
+    # Create a Spotify client using token information
     sp = create_spotify_client(token_info)
     try:
+        # Get the user's playlists
         playlists = sp.current_user_playlists(limit=limit, offset=offset)
         playlists_info = []
         for playlist in playlists["items"]:
+            # Extract playlist information
             playlist_name = playlist["name"]
             playlist_link = playlist["external_urls"]["spotify"]
             image_url = playlist["images"][0]["url"] if playlist["images"] else None
+            # Add playlist information to the list
             playlists_info.append(
                 {"name": playlist_name, "link": playlist_link, "image_url": image_url}
             )
+        # Return the list of user playlists
         return playlists_info
     except Exception as e:
         print(f"Error getting user playlists: {e}")
+        return None
+
+
+# Function to get the user's followers
+def get_user_followers(token_info):
+    # Create a Spotify client using token information
+    sp = create_spotify_client(token_info)
+    try:
+        # Get the user's followers
+        user_info = sp.current_user()
+        followers_count = user_info["followers"]["total"]
+        return followers_count
+    except Exception as e:
+        print(f"Error getting user followers: {e}")
+        return None
+
+
+# Function to get the user's following (people they follow)
+def get_user_following(token_info):
+    # Create a Spotify client using token information
+    sp = create_spotify_client(token_info)
+    try:
+        # Get the user's followed artists
+        followed_artists = sp.current_user_followed_artists()
+        following_count = followed_artists["artists"]["total"]
+        return following_count
+    except Exception as e:
+        print(f"Error getting user following: {e}")
         return None
